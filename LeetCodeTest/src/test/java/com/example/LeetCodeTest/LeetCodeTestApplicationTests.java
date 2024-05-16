614af1e1e6c4b055ed54d0118060acd1ca9e4fc6
terrancen <terrancen@afrosoft.co.zw>
2024-05-16 09:40:16 +0200

    initial commit

---
+++ LeetCodeTest/src/test/java/com/example/LeetCodeTest/LeetCodeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LeetCodeTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetCodeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
